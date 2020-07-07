4d76febf1955fe0a06c409b9f65d045f4dce1b81
gumeik <xzlkong@163.com>
2020-07-07 20:06:47 +0800

    adjust

--- springboot-demo/src/main/resources/static/index.js
+++ springboot-demo/src/main/resources/static/index.js
@@ -1,4 +1,14 @@
 const btn = document.getElementById("btn");
+const dom = document.getElementsByTagName("div")[0];
+let num = 0;
+let colorStr = "";
 btn.onclick = ()=>{
-    alert("hello,springweb")
+    alert("hello,springweb");
+    console.log(dom);
+    console.log(dom.style)
+    num += num;
+    if(num % 2 === 0) colorStr = "red";
+    else colorStr = "green";
+    dom.style.background = colorStr;
+
 }
