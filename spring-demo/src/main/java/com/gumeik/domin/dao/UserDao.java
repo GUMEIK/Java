fbc632329d82c203894d1513cab0361bf91e45bd
gumeik <xzlkong@163.com>
2020-07-08 09:42:43 +0800

    bean注入原始类型的数据

--- spring-demo/src/main/java/com/gumeik/domin/dao/UserDao.java
+++ spring-demo/src/main/java/com/gumeik/domin/dao/UserDao.java
@@ -1,7 +1,14 @@
 package com.gumeik.domin.dao;
 
 public class UserDao {
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public void save(){
+        System.out.println(this.username);
         System.out.println("dao层保存数据！");
     }
 }
