9fe3be6f1787d8c519ec66d24fc3ee40a4866232
gumeik <xzlkong@163.com>
2020-07-08 09:24:12 +0800

    set依赖注入

--- spring-demo/src/main/java/com/gumeik/domin/demo/App.java
+++ spring-demo/src/main/java/com/gumeik/domin/demo/App.java
@@ -1,5 +1,6 @@
 package com.gumeik.domin.demo;
 
+
 import com.gumeik.domin.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,13 +8,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import javax.xml.ws.Service;
 
 public class App {
+
+
     public static void main(String[] args) {
-//        不适用装配
+//  1.      不适用装配
 //        UserService service = new UserService();
 //        service.save();
-//        使用装配
+//  2.      使用装配
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService = (UserService) applicationContext.getBean("userService");
         userService.save();
+
+
+
     }
 }
--- spring-demo/src/main/java/com/gumeik/domin/service/UserService.java
+++ spring-demo/src/main/java/com/gumeik/domin/service/UserService.java
@@ -1,10 +1,19 @@
 package com.gumeik.domin.service;
 
+import com.gumeik.domin.User;
 import com.gumeik.domin.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserService {
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
     public void save(){
-        UserDao userDao = new UserDao();
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("aplicationContext.xml");
+//        UserDao userDao = (UserDao) applicationContext.getBean("user");
         userDao.save();
         System.out.println("service层调用dao层保存了");
     }
