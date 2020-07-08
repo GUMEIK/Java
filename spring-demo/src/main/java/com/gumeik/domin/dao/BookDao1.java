b3a0da6831b28ea6f24a0ec7898f6b159b2cb942
gumeik <xzlkong@163.com>
2020-07-08 10:46:41 +0800

    注解autowiredandqualirier案例

--- spring-demo/src/main/java/com/gumeik/domin/dao/BookDao1.java
+++ spring-demo/src/main/java/com/gumeik/domin/dao/BookDao1.java
@@ -1,7 +1,16 @@
 package com.gumeik.domin.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+//
+//<bean id="bookDao1" class="com.gumeik.domin.dao.BookDao1">
+//    <property name="bookname" value="《白夜行》"></property>
+//</bean>
+@Component("bookDao1")
 public class BookDao1 {
+    @Autowired
+    @Qualifier("bookname")
     @Override
     public String toString() {
         return "BookDao{" +
@@ -9,9 +18,10 @@ public class BookDao1 {
                 '}';
     }
 
+    private String bookname = "月亮与六便士";
     public void setBookname(String bookname) {
         this.bookname = bookname;
     }
 
-    private String bookname = "月亮与六便士";
+
 }
