086905a6a2440d694fd72110d32dbda2d32e1d73
gumeik <xzlkong@163.com>
2020-07-08 10:29:16 +0800

    基本注解@Component

--- spring-demo/src/main/java/com/gumeik/domin/dao/BookDao.java
+++ spring-demo/src/main/java/com/gumeik/domin/dao/BookDao.java
@@ -1,5 +1,9 @@
 package com.gumeik.domin.dao;
 
+import org.springframework.stereotype.Component;
+
+//<bean id="bookDao" class="com.gumeik.domin.dao.BookDao"></bean>
+@Component("bookDao")
 public class BookDao {
     @Override
     public String toString() {
