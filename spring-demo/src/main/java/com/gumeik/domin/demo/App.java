3721a2c83dd6a3ec93e1a5a7102d15e03ba31451
gumeik <xzlkong@163.com>
2020-07-08 09:05:52 +0800

    userservice使用装配

--- spring-demo/src/main/java/com/gumeik/domin/demo/App.java
+++ spring-demo/src/main/java/com/gumeik/domin/demo/App.java
@@ -1,12 +1,19 @@
 package com.gumeik.domin.demo;
 
 import com.gumeik.domin.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.xml.ws.Service;
 
 public class App {
     public static void main(String[] args) {
-        UserService service = new UserService();
-        service.save();
+//        不适用装配
+//        UserService service = new UserService();
+//        service.save();
+//        使用装配
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.save();
     }
 }
